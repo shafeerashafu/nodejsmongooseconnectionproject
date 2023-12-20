57af7d8fdcf8c5aa56fa5df3aee20f46ad8fc846
shafeerashafu <shafeerazahur3@gmail.com>
2023-12-20 23:32:12 +0530

    added secrets in .env file

--- database/mongoose-connection.js
+++ database/mongoose-connection.js
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
+import  dotenv  from "dotenv";
+dotenv.config(); //It loads the varible from .env file into process.env
 
-const localUri="mongodb://127.0.0.1:27017/Userdata";
+console.log("env variables",process.env)
 
+const dbUsername = process.env.DB_USERNAME || "";
+const dbPassword = process.env.DB_PASSWORD || "";
+const dbClustername = process.env.DB_CLUSTER_NAME || "127.0.0.1:27017";
+const dbName = process.env.DB_NAME || "Userdata";
+
+//const localUri=`mongodb://${dbClustername}/${dbName}`;
+
+const cloudUri=`mongodb+srv://${dbUsername}:${dbPassword}@${dbClustername}/${dbName}?retryWrites=true&w=majority`;
 
 
 const connectToDb = async() => {
     try {
-        await mongoose.connect(localUri);
+        await mongoose.connect(cloudUri);
         console.log("Connected to MongoDB");
       } catch (err) {
         console.log("Error connecting to MongoDB", err);
@@ -18,3 +28,8 @@ export default connectToDb;
 
 
 
+//Username for db : shafeerazahur3
+
+//Password for db : BHiB35LxviCHaVY6
+
+//cloudUri : mongodb+srv://shafeerazahur3:<password>@cluster0.3zrhpib.mongodb.net/?retryWrites=true&w=majority
